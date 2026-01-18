6b9aa345c17450b09f329a1ef56025117b92df38
PreetiMoo <preetissmoolya15@gmail.com>
2026-01-18 19:37:39 +0530

    another soln for binary add

--- addBinary.js
+++ addBinary.js
@@ -10,6 +10,80 @@
 // a and b consist only of '0' or '1' characters.
 // Each string does not contain leading zeros except for the zero itself.
 
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+
+
+var addBinary = function (a, b) {
+
+    let carryOver = "0";
+    let result = "";
+    const len = a.length > b.length ? a.length : b.length
+
+    if (a.length != b.length) {
+        let diff = Math.abs(a.length - b.length)
+        if (a.length > b.length) {
+
+            b = "0".repeat(diff) + b
+
+        } else {
+
+            a = "0".repeat(diff) + a
+        }
+
+    }
+
+    for (let i = len - 1; i >= 0; i--) {
+        let l = a[i] || 0
+        let m = b[i] || 0
+        if (l == 1 && m == 1) {
+            if (carryOver == "1") {
+                result += "1"
+            } else {
+                result += "0"
+            }
+            carryOver = "1"
+        } else {
+            if (Number(l) + Number(m) == "1") {
+                if (carryOver == "1") {
+                    carryOver = "1"
+                    result += "0"
+                } else {
+                    carryOver = "0"
+                    result += "1"
+                }
+
+
+            } else {
+                if (carryOver === "1") {
+                    result += Number(l) + Number(m) + Number(carryOver)
+                    carryOver = "0"
+                } else {
+                    result += Number(l) + Number(m)
+                }
+
+            }
+
+
+        }
+
+
+    }
+    if (carryOver == "1") {
+        result += "1"
+        carryOver = "0"
+    }
+    return result.split('').reverse().join('');
+};
+
+
+
+
+
+
 
 var addBinary = function(a, b) {
     let i = a.length - 1;
