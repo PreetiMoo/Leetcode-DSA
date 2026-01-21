bd05ae89e550202e8917868822f07f8dda059f1d
PreetiMoo <preetissmoolya15@gmail.com>
2026-01-21 23:04:54 +0530

    sqrt

---
+++ sqrt.js
@@ -0,0 +1,20 @@
+var mySqrt = function(x) {
+    if (x < 2) return x;
+
+    let left = 1;
+    let right = x;
+    let result = 0;
+
+    for (; left <= right;) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (mid * mid <= x) {
+            result = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return result;
+};
