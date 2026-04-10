ae3686a91a25406d608e5a78c69422e9dc846719
PreetiMoo <preetissmoolya15@gmail.com>
2026-04-10 15:34:27 +0530

    3 Sum using 2 pointer

--- unionSum.js
+++ unionSum.js
@@ -1,3 +1,19 @@
+// Union of 2 Sorted Arrays - 2 pointers
+// Given two sorted arrays a[] and b[], where each array may contain duplicate elements , the task is to return the elements in the union of the two arrays in sorted order.
+// Union of two arrays can be defined as the set containing distinct common elements that are present in either of the arrays.
+
+// Examples:
+
+// Input: a[] = [1, 2, 3, 4, 5], b[] = [1, 2, 3, 6, 7]
+// Output: [1, 2, 3, 4, 5, 6, 7]
+// Explanation: Distinct elements including both the arrays are: 1 2 3 4 5 6 7.
+// Input: a[] = [2, 2, 3, 4, 5], b[] = [1, 1, 2, 3, 4]
+// Output: [1, 2, 3, 4, 5]
+// Explanation: Distinct elements including both the arrays are: 1 2 3 4 5.
+// Input: a[] = [1, 1, 1, 1, 1], b[] = [2, 2, 2, 2, 2]
+// Output: [1, 2]
+// Explanation: Distinct elements including both the arrays are: 1 2.
+
 function uniArr(arr1,arr2){
     let res=[]
   
